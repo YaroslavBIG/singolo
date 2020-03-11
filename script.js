73c8163e53c4119ec206f8e6a450a58aa7b494ec
FireGUN <yaroslavbig@ya.ru>
2020-03-11 23:29:52 +0200

    feet: Swich off screen on click

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ MENU_PORTFOLIO.addEventListener('click', (event) => {
     MENU_PORTFOLIO.querySelectorAll("li").forEach(el => el.classList.remove('porfolio__menu_active'))
     event.target.classList.add('porfolio__menu_active');
 });
+
+
+
+const IPHONE_H = document.getElementById('base_horisontal');
+const base_horisontal = document.querySelector("#base_horisontal > img");
+IPHONE_H.addEventListener('click', (event) => {
+    base_horisontal.classList.contains('base-background_hover') ? event.target.classList.remove('base-background_hover') : event.target.classList.add('base-background_hover');
+});
+
+
+const IPHONE_V = document.getElementById('base_vertical');
+const base_vertical = document.querySelector("#base_vertical > img");
+IPHONE_V.addEventListener('click', (event) => {
+    base_vertical.classList.contains('base-background_hover') ? event.target.classList.remove('base-background_hover') : event.target.classList.add('base-background_hover');
+});
