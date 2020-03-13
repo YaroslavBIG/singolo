0d1be478739d654e2b2b765b9861b334d3175f43
FireGUN <yaroslavbig@ya.ru>
2020-03-13 22:44:43 +0200

    feet: change slide on click

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const MENU = document.getElementById("header__menu");
+const MENU = document.getElementById('header__menu');
 
 MENU.addEventListener('click', (event) => {
-    MENU.querySelectorAll("a").forEach(el => el.classList.remove('header__menu_active'))
+    MENU.querySelectorAll('a').forEach(el => el.classList.remove('header__menu_active'))
     event.target.classList.add('header__menu_active');
 });
 
-const MENU_PORTFOLIO = document.getElementById("porfolio_menu");
+const MENU_PORTFOLIO = document.getElementById('porfolio_menu');
 
 MENU_PORTFOLIO.addEventListener('click', (event) => {
-    MENU_PORTFOLIO.querySelectorAll("li").forEach(el => el.classList.remove('porfolio__menu_active'))
+    MENU_PORTFOLIO.querySelectorAll('li').forEach(el => el.classList.remove('porfolio__menu_active'))
     event.target.classList.add('porfolio__menu_active');
 });
 
@@ -20,7 +20,7 @@ const getQuerySelector = (el) => document.querySelector(el);
 const eventRemove = (el) => event.target.classList.remove(el);
 const eventAdd  = (el) => event.target.classList.add(el);
 const displayHidden = 'base-background_hover';
-
+// Screen off
 getElById('base_horisontal').addEventListener('click', (event) => {
     getQuerySelector('#base_horisontal > img').classList.contains(displayHidden) ? eventRemove(displayHidden) : eventAdd(displayHidden);
 });
@@ -41,3 +41,24 @@ getElById('base__small_right').addEventListener('click', (event) => {
     getQuerySelector('#base__small_right > img').classList.contains(displayHidden) ? eventRemove(displayHidden) : eventAdd(displayHidden);
 });
 
+// Slider
+const screenSize = 1022;
+const slider = getElById('slider');
+const slider_next = getElById('slider_next');
+getElById('button_left').addEventListener('click', (event) => {
+    console.log('click left');
+    slider.classList.toggle("hidden");
+    slider_next.classList.toggle("hidden");
+    //for (let count = 0; count <= screenSize; count += 1) {
+    //    getElById('slider').style.right = `${count}px`
+    //}
+});  
+
+getElById('button_right').addEventListener('click', (event) => {
+    console.log('click right')
+    slider.classList.toggle("hidden");
+    slider_next.classList.toggle("hidden");
+    //for (let count = 0; count < screenSize; count += 1) {
+    //  getElById('slider').style.left = `${count}px`
+    //}
+});  
